0e357de0c54f1c7c7e35b3d8e3ecd69a0ded4ce0
Marco Mendez <marcomendez1796@hotmail.com>
2020-08-18 03:05:37 -0600

    Configuracion de controladores basicos

---
+++ Controllers/LoanHistoriesController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AlquilerDeVideoJuegos_History.Models;
+
+namespace AlquilerDeVideoJuegos_History.Controllers
+{
+    public class LoanHistoriesController : Controller
+    {
+        private readonly MyDBContext _context;
+
+        public LoanHistoriesController(MyDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: LoanHistories
+        public async Task<IActionResult> Index()
+        {
+            var myDBContext = _context.LoanHistory.Include(l => l.Client).Include(l => l.VideoGame);
+            return View(await myDBContext.ToListAsync());
+        }
+
+        // GET: LoanHistories/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var loanHistory = await _context.LoanHistory
+                .Include(l => l.Client)
+                .Include(l => l.VideoGame)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (loanHistory == null)
+            {
+                return NotFound();
+            }
+
+            return View(loanHistory);
+        }
+
+        // GET: LoanHistories/Create
+        public IActionResult Create()
+        {
+            ViewData["ClientId"] = new SelectList(_context.Client, "Id", "Id");
+            ViewData["VideoGameId"] = new SelectList(_context.VideoGame, "Id", "Id");
+            return View();
+        }
+
+        // POST: LoanHistories/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,LondDate,LondDateTime,CreationDate,ClientId,VideoGameId")] LoanHistory loanHistory)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(loanHistory);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClientId"] = new SelectList(_context.Client, "Id", "Id", loanHistory.ClientId);
+            ViewData["VideoGameId"] = new SelectList(_context.VideoGame, "Id", "Id", loanHistory.VideoGameId);
+            return View(loanHistory);
+        }
+
+        // GET: LoanHistories/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var loanHistory = await _context.LoanHistory.FindAsync(id);
+            if (loanHistory == null)
+            {
+                return NotFound();
+            }
+            ViewData["ClientId"] = new SelectList(_context.Client, "Id", "Id", loanHistory.ClientId);
+            ViewData["VideoGameId"] = new SelectList(_context.VideoGame, "Id", "Id", loanHistory.VideoGameId);
+            return View(loanHistory);
+        }
+
+        // POST: LoanHistories/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,LondDate,LondDateTime,CreationDate,ClientId,VideoGameId")] LoanHistory loanHistory)
+        {
+            if (id != loanHistory.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(loanHistory);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LoanHistoryExists(loanHistory.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClientId"] = new SelectList(_context.Client, "Id", "Id", loanHistory.ClientId);
+            ViewData["VideoGameId"] = new SelectList(_context.VideoGame, "Id", "Id", loanHistory.VideoGameId);
+            return View(loanHistory);
+        }
+
+        // GET: LoanHistories/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var loanHistory = await _context.LoanHistory
+                .Include(l => l.Client)
+                .Include(l => l.VideoGame)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (loanHistory == null)
+            {
+                return NotFound();
+            }
+
+            return View(loanHistory);
+        }
+
+        // POST: LoanHistories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var loanHistory = await _context.LoanHistory.FindAsync(id);
+            _context.LoanHistory.Remove(loanHistory);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LoanHistoryExists(int id)
+        {
+            return _context.LoanHistory.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Migrations/20200818083949_First-Migration.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AlquilerDeVideoJuegos_History.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Client",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Adress = table.Column<string>(nullable: true),
+                    Telephone = table.Column<string>(nullable: true),
+                    IsActive = table.Column<bool>(nullable: false),
+                    CreationDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Client", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VideoGame",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    TypeGame = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VideoGame", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LoanHistory",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    LondDate = table.Column<int>(nullable: false),
+                    LondDateTime = table.Column<DateTime>(nullable: false),
+                    CreationDate = table.Column<DateTime>(nullable: false),
+                    ClientId = table.Column<int>(nullable: false),
+                    VideoGameId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LoanHistory", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LoanHistory_Client_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Client",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_LoanHistory_VideoGame_VideoGameId",
+                        column: x => x.VideoGameId,
+                        principalTable: "VideoGame",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LoanHistory_ClientId",
+                table: "LoanHistory",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LoanHistory_VideoGameId",
+                table: "LoanHistory",
+                column: "VideoGameId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LoanHistory");
+
+            migrationBuilder.DropTable(
+                name: "Client");
+
+            migrationBuilder.DropTable(
+                name: "VideoGame");
+        }
+    }
+}
